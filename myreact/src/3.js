7acf76279aca09cafbe004a80f3c4535d57bdc82
zhaodanli <3489966805@qq.com>
2018-07-02 13:43:32 +0800

    校验

--- myreact/src/3.js
+++ myreact/src/3.js
@@ -1,6 +1,47 @@
 import  React , {Component} from 'react'; //创建元素
 import {render} from 'react-dom'; //渲染页面dom
+import PropTypes from 'prop-types'; 
 
-function clock(){
-    return <h1></h1>
+// 第1行：React.PropTypes自React 15.5.0开始不推荐使用
+//而是使用npm模块prop-types代替react/no-deprecated'PropTypes'已定义，但从未使用过no-unused-vars
+
+class Person extends Component{
+    //Es6不支持静态属性, 只有静态方法
+    static PropTypes = {
+        name: PropTypes.string.isRequired ,
+        age : PropTypes.number,
+        gender:  PropTypes.oneOf ([ '男' ,'女']),
+        hobby :  PropTypes.arrayOf([PropTypes.string]),
+        pos:  PropTypes.shape({
+            x: PropTypes.number,
+            y: PropTypes.number
+        })
+    }
+    // static defaultProps = {
+    //     name:'zdl'
+    // }
+    constructor(props){
+        super();
+        // this.state = {data: ''}
+    }
+
+    render(){
+        return (<h1>
+            {this.props.name}
+        </h1>)
+    }
+   
 }
+let person = {
+    name : 'person' ,
+    age : 20 ,
+    gender: '' ,
+    hobby : ['吃饭', '睡觉'],
+    pos: {
+        x:100,
+        y:200
+    }
+
+}
+
+render(<Person {...person}></Person>, window.root)
